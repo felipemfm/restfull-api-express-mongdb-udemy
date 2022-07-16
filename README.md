8a29e0ad4e1d34df968bd440445a93596a41b836
felipemfm <felipemfm@gmail.com>
2022-07-16 15:27:14 +0900

    first commit.

---
+++ README.md
@@ -0,0 +1,11 @@
+# About
+
+This RESTfull API project is the result of following the [Node.js, Express, MongoDB & More: The Complete Bootcamp 2022](https://www.udemy.com/course/nodejs-express-mongodb-bootcamp/) online course at Udemy. 
+
+Author and course instructor is [Jonas Schmedtmann](https://www.udemy.com/course/nodejs-express-mongodb-bootcamp/#instructor-1) and this file represent around 90% of the course.
+
+## Technologies Overview
+
+* Node/Express
+* MongoDB
+* Mongoose
---
+++ app.js
@@ -0,0 +1,103 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+const cookieParser = require('cookie-parser');
+
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
+const viewRouter = require('./routes/viewRoutes');
+
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+// GLOBAL MIDDLEWARES
+// Serving static files
+app.use(express.static(path.join(__dirname, 'public')));
+// Set Security HTTP headers
+app.use(
+  helmet({
+    crossOriginEmbedderPolicy: false,
+    contentSecurityPolicy: {
+      directives: {
+        'child-src': ['blob:'],
+        'connect-src': ['https://*.mapbox.com'],
+        'default-src': ["'self'"],
+        'font-src': ["'self'", 'https://fonts.gstatic.com'],
+        'img-src': ["'self'", 'data:', 'blob:'],
+        'script-src': ["'self'", 'https://*.mapbox.com'],
+        'style-src': ["'self'", 'https:'],
+        'worker-src': ['blob:'],
+      },
+    },
+  })
+);
+
+// Devlopment logging
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// LImit requets from same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests for this IP, please try again in an hour',
+});
+app.use('/api', limiter);
+
+// Body parser, reading data from the body into req.body
+app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+app.use(cookieParser());
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsAverage',
+      'ratingsQuantity',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+);
+
+// Test Middleware
+app.use((req, res, next) => {
+  req.requestTIme = new Date().toISOString();
+  // console.log(req.cookies);
+  next();
+});
+
+// ROUTES
+
+app.use('/', viewRouter);
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/reviews', reviewRouter);
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
+});
+
+app.use(globalErrorHandler);
+
+module.exports = app;
